910fd144d416082ccfcbad5b1af4abfa30201cef
TimKuijpers <tim.kuijpers@maastrichtuniversity.nl>
2020-04-23 11:44:28 +0200

    Final version

---
+++ getClusterWithSample.py
@@ -0,0 +1,10 @@
+import numpy as np
+def getclusterwithsample(input = None,columnMembers = None,columnCluster = None):
+    maxClusters = np.max(input[columnCluster])
+    print(maxClusters)
+    clustersInData=range(1,maxClusters+1)
+    clustersWithSamples=dict()
+    for x in clustersInData:
+        samples=input[input[columnCluster]==x]
+        clustersWithSamples["cluster"+str(x)]=samples
+    return clustersWithSamples
