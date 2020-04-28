a05372adbe04b7f7b496ccb597e4dc87cdddc9b1
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2020-04-28 17:12:40 +0200

    Extract clusters to one data frame

--- getClusterWithSample.py
+++ getClusterWithSample.py
@@ -1,7 +1,7 @@
 import numpy as np
+import pandas as pd
 def getclusterwithsample(input = None,columnMembers = None,columnCluster = None):
     maxClusters = np.max(input[columnCluster])
-    print(maxClusters)
     clustersInData=range(1,maxClusters+1)
     clustersWithSamples=dict()
     for x in clustersInData:
@@ -9,5 +9,19 @@ def getclusterwithsample(input = None,columnMembers = None,columnCluster = None)
         clustersWithSamples["cluster"+str(x)]=samples
     return clustersWithSamples
 
-def createdataframeclusters(input = None, columnMembers= None, columnCluster = None):
-    return None
+def createdataframeclusters(input = None,columnNames=None):
+    # First for each cluster create an extra column with "Cluster x "  as info
+    dataFrameReturn=[]
+    for y in range(1,input.__len__()+1):
+        for x in input:
+            if columnNames==None:
+                clusterNumber="Cluster "+str(y)
+                dataFrame=x.assign(Cluster=clusterNumber)
+                dataFrameReturn.append(dataFrame)
+            else:
+                x=x.loc[:,columnNames]
+                clusterNumber="Cluster "+str(y)
+                dataFrame=x.assign(Cluster=clusterNumber)
+                dataFrameReturn.append(dataFrame)
+    dataFrameCluster=pd.concat(dataFrameReturn,axis=0)
+    return dataFrameCluster
