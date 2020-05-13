82bedd56c53d7c55b471d6c9ea125a0b43d768e8
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2020-05-13 15:14:59 +0200

    Update for arrays

--- getClusterWithSample.py
+++ getClusterWithSample.py
@@ -1,5 +1,48 @@
 import numpy as np
 import pandas as pd
+
+def getconsensusmatrixfromhmatrix(input=None,sampleNames=None):
+    consensusmatrixH=consensusmatrix(Hdata=input, samplenames=sampleNames)
+    return consensusmatrixH
+
+def consensusMatrix(Hdata, samplenames):
+    CoAssociationMatrices = []
+    for x in range(len(Hdata)):
+        MembersOfClusters, occurenceCluster = determineClusterMembers(Hdata[x], samplenames)
+        CoAssociationMatrix = coAssociationMatrix(occurenceCluster)
+        CoAssociationMatrices.append(CoAssociationMatrix)
+        Consensusmatrix = sum(CoAssociationMatrices)
+        Consensusmatrix = Consensusmatrix / len(Hdata)
+
+    return Consensusmatrix
+
+
+def coAssociationMatrix(clusterlist):
+    results = [[int(x == y) for y in clusterlist] for x in clusterlist]
+    coAssociation = np.array(results)
+    return coAssociation
+
+def determineClusterMembers(Hmatrix, samplenames):
+    "For each column in H, maximum value will be determined and right cluster assigned"
+    index_maxvalue = np.argmax(Hmatrix, axis=0)
+    ClusterMembers = []
+    for cluster in range(np.min(index_maxvalue), np.max(index_maxvalue) + 1):
+        ClusterMembers.append([i for indx, i in enumerate(samplenames) if index_maxvalue[indx] == cluster])
+    return ClusterMembers, index_maxvalue
+
+def getclustermembersfromarray(input=None):
+    dataframeClusters=[]
+    dataFrameList=[]
+    k=1
+    for x in input:
+        tmpDataFrame=pd.DataFrame({'Sample':x})
+        tmpDataFrame=tmpDataFrame.assign(Cluster='cluster '+str(k))
+        dataFrameList.append(tmpDataFrame)
+        k=k+1
+    dataframeClusters=pd.concat(dataFrameList,axis=0)
+    return dataframeClusters
+
+
 def getclusterwithsample(input = None,columnMembers = None,columnCluster = None):
     maxClusters = np.max(input[columnCluster])
     clustersInData=range(1,maxClusters+1)
