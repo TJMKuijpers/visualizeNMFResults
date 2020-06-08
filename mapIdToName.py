148e827c5d64758a175103f388036a648c03e266
Tim Kuijpers <33220871+TJMKuijpers@users.noreply.github.com>
2020-06-08 16:38:43 +0200

    Update
    Update to convert gene symbols back to CpG sites

--- mapIdToName.py
+++ mapIdToName.py
@@ -1,9 +1,17 @@
 import pandas as pd
 
 
-def mapidtoname(idstomap=None, database=None,idColumn=None):
-    idsMappedToDataBase=database[database[idColumn].isin(idstomap)]
+def mapidtoname(idstomap=None, database=None, idcolumn=None):
+    idsMappedToDataBase=database[database[idcolumn].isin(idstomap)]
     return idsMappedToDataBase
+
+
+def mapidtocpgvariable(idstomap=None, database=None, idcolumn=None, columntoreturn=None):
+    # get the gene ids that are mapped to the database
+    idstoextract = database[database[idcolumn].isin(idstomap)]
+    # return the CpG variable for that gene
+    idstoreturn = idstoextract[columntoreturn]
+    return idstoreturn
 
 
 def setthedatabase(pathtodatabase=None, seperator=None):
@@ -11,6 +19,6 @@ def setthedatabase(pathtodatabase=None, seperator=None):
     return database
 
 
-def mapgenenamestoctd(genenames=None,ctdDataBase=None,columnId=None):
-    idsMappedToCtd=ctdDataBase[ctdDataBase[columnId].isin(genenames)]
-    return idsMappedToCtd
+def mapgenenamestoctd(genenames=None, ctddatabase=None, columnid=None):
+    idsmappedtoctd = ctddatabase[ctddatabase[columnid].isin(genenames)]
+    return idsmappedtoctd
